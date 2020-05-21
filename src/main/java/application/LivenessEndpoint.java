de1cc116bb6707d6adf5c2ea404d7c59abbb221a
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-05-21 12:32:02 -0500

    moved the LivenessEndpoint class

--- src/main/java/application/LivenessEndpoint.java
+++ src/main/java/application/LivenessEndpoint.java
@@ -1,4 +1,4 @@
-package application.controller;
+package application;
 
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
